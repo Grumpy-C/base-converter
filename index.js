8a7bf94ec179d2bad6ffb07254c2137ae4ec845f
Grumpy-C <111717366+Grumpy-C@users.noreply.github.com>
2024-01-19 08:07:14 +1300

    basic features, javascript (maybe?) completed, need to debug

---
+++ index.js
@@ -0,0 +1,58 @@
+/*
+TODO: debug and add sint support
+*/
+
+const alphabet = "0123456789abcdefghijklmnopqrstuvwxyz";
+
+const inp = document.getElementById("inp");
+const initbase = document.getElementById("initbase");
+const newbase = document.getElementById("newbase");
+const submit = document.getElementById("submit");
+const out = document.getElementById("out")
+
+submit.addEventListener("click", () => {
+    try {
+        let inpval = inp.value.toLowerCase;
+        let initbaseval = Number(initbase.value);
+        let newbaseval = Number(newbase.value);
+
+        if (isNaN(newbaseval) || isNaN(initbaseval)) {throw "Base is not a number"}
+
+        let base10num = 0;
+        let output = "";
+
+        //DEBUG 1 START
+            conosle.log("inpval = " + inpval)
+            conosle.log("initbaseval = " + initbaseval)
+            conosle.log("newbaseval = " + newbaseval)
+        //DEBUG 1 END
+
+        for (let index = 0; index < inpval.length; index++) {
+            base10num += alphabet.indexOf(inpval[index]) * initbaseval**index
+        }
+
+        //DEBUG 2 START
+            conosle.log("base10num = " + base10num)
+        //DEBUG 2 END
+
+        if (newbaseval != 10) {
+            while (base10num != 0) {
+                output += alphabet[base10num % newbaseval]
+                base10num = Math.floor(base10num/newbaseval)
+            }
+        } else {
+            output = base10num
+            base10num = 0
+        }
+
+        //DEBUG 3 START
+            conosle.log("base10num = " + base10num)
+            conosle.log("output = " + output)
+        //DEBUG 3 END
+
+        out.innerText = output
+
+    } catch(err) {
+        alert("An error occured:" + err.message);
+    }
+});

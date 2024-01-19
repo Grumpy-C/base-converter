c2a78e99ba847cce33d8c139a524f1651619e125
Grumpy-C <111717366+Grumpy-C@users.noreply.github.com>
2024-01-19 13:29:41 +1300

    minor changes to code, trying to modify the layout

--- index.js
+++ index.js
@@ -17,48 +17,52 @@ window.addEventListener("DOMContentLoaded", () => {
             let inpval = inp.value.toLowerCase();
             let initbaseval = Number(initbase.value);
             let newbaseval = Number(newbase.value);
-    
-            if (isNaN(newbaseval) || isNaN(initbaseval)) {throw "Base is not a number"}
-            if (initbaseval < 2 || initbaseval > 36 || newbaseval < 2 || newbaseval > 36) {throw "Invalid base"}
+
+            if (isNaN(newbaseval) || isNaN(initbaseval)) {
+                throw "Base is not a number";
+            };
+            if (initbaseval < 2 || initbaseval > 36 || newbaseval < 2 || newbaseval > 36) {
+                throw "Invalid base"
+            };
             
             let base10num = 0;
             let output = "";
     
             //DEBUG 1 START
-                console.log("inpval = " + inpval)
-                console.log("initbaseval = " + initbaseval)
-                console.log("newbaseval = " + newbaseval)
+                console.log("inpval = " + inpval);
+                console.log("initbaseval = " + initbaseval);
+                console.log("newbaseval = " + newbaseval);
             //DEBUG 1 END
             
             // turn the number into base 10
             for (let index = 0; index < inpval.length; index++) {
-                base10num += alphabet.indexOf(inpval[index]) * initbaseval**(inpval.length - index - 1)
+                base10num += alphabet.indexOf(inpval[index]) * initbaseval**(inpval.length - index - 1);
             }
     
             //DEBUG 2 START
-                console.log("base10num = " + base10num)
+                console.log("base10num = " + base10num);
             //DEBUG 2 END
             
             // turn it into it's new base
             if (newbaseval != 10) {
                 while (base10num != 0) {
-                    output = alphabet[base10num % newbaseval] + output
-                    base10num = Math.floor(base10num/newbaseval)
+                    output = alphabet[base10num % newbaseval] + output;
+                    base10num = Math.floor(base10num/newbaseval);
                 }
             } else {
                 output = base10num
                 base10num = 0
-            }
+            };
     
             //DEBUG 3 START
-                console.log("base10num = " + base10num)
-                console.log("output = " + output)
+                console.log("base10num = " + base10num);
+                console.log("output = " + output);
             //DEBUG 3 END
     
             out.value = output
     
         } catch(err) {
             alert("An error occured: " + err.message);
-        }
+        };
     });
 });
